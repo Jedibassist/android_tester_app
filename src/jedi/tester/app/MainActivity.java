c4b56602eceb177d9e3fc1630a61e1d18ebcedae
Jed Gravelin <jed.s.gravelin@gmail.com>
2012-07-19 10:36:23 -0400

    Made this an HTML5 / WebApp tester app.
    Seems pretty cool!

--- src/jedi/tester/app/MainActivity.java
+++ src/jedi/tester/app/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
@@ -12,6 +14,10 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl(getString(R.string.file_android_asset_www_index_html));
     }
 
     @Override
